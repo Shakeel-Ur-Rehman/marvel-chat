d549b5488d7d326b8cdca9f9e878f42f5f8a60cb
Usamariaz175 <usamariaz175@gmail.com>
2019-06-20 19:02:12 +0500

    add new modules

--- src/components/SideBarRight.js
+++ src/components/SideBarRight.js
@@ -1,16 +1,46 @@
 import React, { Component } from 'react'
 import InboxDetail from './InboxDetail';
+import { Container } from 'react-bootstrap';
+import Switch from "react-switch";
 
 export default class SideBarRight extends Component {
     render() {
         return (
-            <div style={{background:"white",height:"550px"}}>
+            <div>
             <div style={{display:"flex",flexDirection:"column"}}>
             <InboxDetail/>
-            <div>Take Ownership</div>
-            <div>Other Details</div>
+            <div>
+            <div  style={{background:"white",height:"52px",padding:"10px",marginBottom:"10px"}}>
+                <p style={{float:"left"}}>Take Ownership</p>
+                <div style={{float:"right"}}>
+                <Switch
+                        checked={false}
+                        onColor="#86d3ff"
+                        onHandleColor="#2693e6"
+                        handleDiameter={20}
+                        uncheckedIcon={false}
+                        checkedIcon={false}
+                        boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+                        activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+                        height={15}
+                        width={35}
+                        className="react-switch"
+                        id="material-switch"
+                />
             </div>
-        </div>
+            </div>
+            </div>
+            <div style={{background:"white"}}>
+            <Container>
+            <h5 style={{textAlign:"center",margin:"20px 0px"}}>Other Infromation</h5>
+            <div style={{background:"#E9EBEE",height:"180px",marginBottom:"5px"}}></div>
+            <div style={{background:"#E9EBEE",height:"180px",marginBottom:"5px"}}></div>
+            <div style={{background:"#E9EBEE",height:"180px",marginBottom:"5px"}}></div>
+            <div style={{background:"#E9EBEE",height:"180px",marginBottom:"5px"}}></div>
+            </Container>
+            </div>
+            </div>
+        </div >
         )
     }
 }
