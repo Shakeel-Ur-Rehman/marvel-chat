76b428df24f2e14bbea597fedf8429b71a81ec82
Usamariaz175 <usamariaz175@gmail.com>
2019-06-21 18:06:37 +0500

    create state and handiling functions

--- src/lib/redux/Reducers/simpleReducers.js
+++ src/lib/redux/Reducers/simpleReducers.js
@@ -1,11 +1,31 @@
 const initialstate={
-    name:"shakeel"
+    inbox:[
+        {
+            id:1,name:"Usama",phone:"+92 300 1234987",email:"usama123@gmail.com",messages:["Usama this is your first message","Usama this is your second message"]
+        },
+        {
+            id:2,name:"Shakeel",phone:"+92 334 1234987",email:"shakeel456@gmail.com",messages:["Shakeel this is your first message","Shakeel this is your second message"]
+        },
+        {
+            id:3,name:"Mohsin",phone:"+92 343 1234987",email:"mohsin789@gmail.com",messages:["Mohsin this is your first message","Mohsin this is your second message"]
+        }
+    ],
+    active_inbox:1,
+    active_ownership:false
+
+    
 }
 export default (state = initialstate, action) => {
     switch (action.type) {
-     case 'SIMPLE_ACTION':
+     case 'ChangeActiveInbox':
       return {
-       result: action.payload
+          ...state,
+            active_inbox:action.id
+      }
+      case 'ChangeActiveOwnership':
+      return {
+          ...state,
+            active_ownership:!state.active_ownership
       }
      default:
       return state
