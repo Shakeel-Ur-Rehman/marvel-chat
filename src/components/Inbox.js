b302b939682cc52039481011fa9e2dddb6d78d1e
Usamariaz175 <usamariaz175@gmail.com>
2019-06-21 18:04:20 +0500

    handiling state

--- src/components/Inbox.js
+++ src/components/Inbox.js
@@ -1,48 +1,39 @@
 import React from 'react'
 import { Container } from 'react-bootstrap';
+import {connect} from 'react-redux'
 
-export default function Inbox() {
-    return (
-        <div>
+ function Inbox(props) {
+     console.log(props)
+    return (  
         <Container fluid={true}>
-        <div style={{height:"75px",borderBottom:"1px solid black",backgroundColor:"rgba(233,235,238,0.5)"}}>
-            <div style={{float:"left",width:"20%",marginRight:"2%"}}>
-            <div style={{width:"50px",margin:"10px 5px 0px 0px",height:"50px",background:"#E5E9F2",borderRadius:"25px"}}></div>
+        {
+            props.inbox.map(value=>
+                
+                <div key={value.id} onClick={()=>props.changeActiveInbox(value.id)}  style={props.active==value.id?{height:"75px",borderBottom:"1px solid black",backgroundColor:"rgba(233,235,238,0.5)"}:{height:"75px",borderBottom:"1px solid black"}}>
+                <div style={{float:"left",width:"20%",marginRight:"2%"}}>
+                <div style={{width:"50px",margin:"10px 5px 0px 0px",height:"50px",background:"#E5E9F2",borderRadius:"25px"}}></div>
+                </div>
+                <div style={{float:"left",width:"77%",marginTop:"8px"}}>
+                    <h5 style={{marginBottom:"unset",fontSize:"18px"}}>{value.name}</h5>
+                    <p style={{fontSize:"14px",marginBottom:"unset"}}>{value.messages[0]}</p>
+                </div>
             </div>
-            <div style={{float:"left",width:"77%",marginTop:"8px"}}>
-                <h5 style={{marginBottom:"unset",fontSize:"18px"}}>John Doe</h5>
-                <p style={{fontSize:"14px",marginBottom:"unset"}}>lorem ipsum dollar iset collection</p>
-            </div>
-        </div>
-        <div style={{height:"75px",borderBottom:"1px solid black",background:"white"}}>
-            <div style={{float:"left",width:"20%",marginRight:"2%"}}>
-            <div style={{width:"50px",margin:"10px 5px 0px 0px",height:"50px",background:"#E5E9F2",borderRadius:"25px"}}></div>
-            </div>
-            <div style={{float:"left",width:"77%",marginTop:"8px"}}>
-                <h5 style={{marginBottom:"unset",fontSize:"18px"}}>Eugene Lawson</h5>
-                <p style={{fontSize:"14px",marginBottom:"unset"}}>lorem ipsum dollar iset collection</p>
-            </div>
-        </div>
-        <div style={{height:"75px",borderBottom:"1px solid black",background:"white"}}>
-            <div style={{float:"left",width:"20%",marginRight:"2%"}}>
-            <div style={{width:"50px",margin:"10px 5px 0px 0px",height:"50px",background:"#E5E9F2",borderRadius:"25px"}}></div>
-            </div>
-            <div style={{float:"left",width:"77%",marginTop:"8px"}}>
-                <h5 style={{marginBottom:"unset",fontSize:"18px"}}>John Doe</h5>
-                <p style={{fontSize:"14px",marginBottom:"unset"}}>lorem ipsum dollar iset collection</p>
-            </div>
-        </div>
-        <div style={{height:"75px",borderBottom:"1px solid black",background:"white"}}>
-            <div style={{float:"left",width:"20%",marginRight:"2%"}}>
-            <div style={{width:"50px",margin:"10px 5px 0px 0px",height:"50px",background:"#E5E9F2",borderRadius:"25px"}}></div>
-            </div>
-            <div style={{float:"left",width:"77%",marginTop:"8px"}}>
-                <h5 style={{marginBottom:"unset",fontSize:"18px"}}>Eugene Lawson</h5>
-                <p style={{fontSize:"14px",marginBottom:"unset"}}>lorem ipsum dollar iset collection</p>
-            </div>
-        </div>
-        
-</Container>
-</div>
+            )
+        }    
+        </Container>
     )
 }
+function mapStateToProps(state) {
+    return {
+    inbox : state.simpleReducer.inbox,
+    active: state.simpleReducer.active_inbox
+    };
+  }
+function mapDispatchToProps(dispatch){
+    return{
+        changeActiveInbox:(id)=>dispatch({type:"ChangeActiveInbox",id})
+    }
+  }
+  
+  export default connect(mapStateToProps,mapDispatchToProps)(Inbox);
+ 
