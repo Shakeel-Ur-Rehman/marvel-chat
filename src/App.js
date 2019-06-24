d1669dad3b244d18d69d49f20ebb4dee3b7925f2
shakeel <shakeel.rehman@pikessoft.com>
2019-06-24 19:14:31 +0500

    added profile page route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Conversations from './containers/Conversations';
 import Members from "./containers/Members"
 import LiveScritps from "./containers/LiveScripts"
 import Login from "./containers/Login"
+import  Profile from "./containers/Profile"
 function App() {
   return (
     <div className="App">
@@ -12,6 +13,7 @@ function App() {
       <Header/>
       <Switch>
       <Route exact path="/" component={Conversations}/>
+      <Route exact path="/profile" component={Profile}/>
       <Route exact path="/login" component={Login}/>
       <Route exact path="/conversations" component={Conversations}/>
       <Route exact path="/members" component={Members}/>
