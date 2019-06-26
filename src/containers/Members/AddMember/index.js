39efaf710e9f2b01f581330e66f8a0f8276cb74c
Usamariaz175 <usamariaz175@gmail.com>
2019-06-26 19:33:12 +0500

    add member functionality

--- src/containers/Members/AddMember/index.js
+++ src/containers/Members/AddMember/index.js
@@ -1,14 +1,20 @@
 import React from "react"
 import "./style.css"
 import {connect} from "react-redux"
-import {SET_NEW_MEMBER} from '../../../lib/redux/Actions/membersAction'
+import {SET_NEW_MEMBER,Add_Member} from '../../../lib/redux/Actions/membersAction'
 import {Modal,InputGroup,FormControl,DropdownButton,Dropdown, Button} from "react-bootstrap"
 class index extends React.Component {
+  constructor(props){
+    super(props);
+  }
+ 
     render() {
-      console.log(this.props.new_member)
+      console.log(this.props.newmember)
       return (
         <Modal
-          {...this.props}
+          animation={true}
+          onHide={this.props.onHide}
+          show={this.props.show}
           size="md"
           aria-labelledby="contained-modal-title-vcenter"
           centered
@@ -25,7 +31,7 @@ class index extends React.Component {
                 <FormControl
                 name="name"
                 value={this.props.newmember.name}
-                onChange={(e)=>this.props.set_new_message(e.target.name,e.target.value)}
+                onChange={(e)=>{this.props.set_member(e.target.name,e.target.value)}}
                 placeholder="Name"
                 aria-label="Name"
                 aria-describedby="basic-addon2"
@@ -36,27 +42,31 @@ class index extends React.Component {
             <InputGroup className="mb-3">
                 <FormControl
                 name="email"
-                value={this.props.newmember.email}
                 placeholder="Email"
-                onChange={(e)=>this.props.set_new_message(e.target.name,e.target.value)}
+                value={this.props.newmember.email}
                 aria-label="Email"
                 aria-describedby="basic-addon2"
+                onChange={(e)=>{this.props.set_member(e.target.name,e.target.value)}}
                 className="Input"
                 />
             </InputGroup>
-            <InputGroup className="mb-3" style={{width:"100%"}}>
-                <DropdownButton
-                name="role"
-                value={this.props.newmember.role}
-                className="RoleDropDown"
-                variant="outline-secondary"
-                title="Role"
-                id="mybutton"
-                >
-                <Dropdown.Item>Action</Dropdown.Item>
-                </DropdownButton>
+            
+            <InputGroup className="mb-3" style={{width:"100%",background:"ffffff"}}>
+            <Dropdown name="role" className="RoleDropDown"  onSelect={(e)=>{this.props.set_member("role",e)}}>
+              <Dropdown.Toggle id="mybutton"  >
+              {this.props.newmember.role}
+                </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item eventKey="Admin">Admin</Dropdown.Item>
+                <Dropdown.Item eventKey="Super Admin">Super Admin</Dropdown.Item>
+                <Dropdown.Item eventKey="Manager">Manager</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
             </InputGroup>
-                <Button id="CreateButton">
+                <Button id="CreateButton" onClick={()=>{this.props.add_member();
+                this.props.onHide()
+                }}>
                     Create
                 </Button>
   </div>
@@ -65,6 +75,7 @@ class index extends React.Component {
       );
     }
   }
+ 
   const mapStateToProps=(state)=>{
     return{
       newmember:state.membersReducer.newMember
@@ -72,7 +83,8 @@ class index extends React.Component {
   }
   const mapDispatchToProps=(disaptch)=>{
     return{
-      set_new_message:(key,value)=>disaptch({type:SET_NEW_MEMBER,key,value})
+      set_member:(key,value)=>disaptch({type:SET_NEW_MEMBER,key,value}),
+      add_member:()=>disaptch({type:Add_Member})
     }
   }
   export default connect(mapStateToProps,mapDispatchToProps)(index)
--- src/lib/redux/Reducers/membersReducer.js
+++ src/lib/redux/Reducers/membersReducer.js
@@ -2,28 +2,44 @@ import {Add_Member,Remove_Member,SET_NEW_MEMBER} from '../Actions/membersAction'
 const initialstate={
     members:[
         {
-           id:1, name:"shakeel",email:"shakeelzain89@gmail.com", contact:"+92 310 5067211",role:"admin"
+           id:1, name:"shakeel",email:"shakeelzain89@gmail.com", contact:"+92 310 5067211",role:"Admin"
         },
         {
-            id:2, name:"Usama",email:"usmama89@gmail.com", contact:"+92 310 7667211",role:"superadmin"
+            id:2, name:"Usama",email:"usmama89@gmail.com", contact:"+92 310 7667211",role:"Super Admin"
          },
          {
-            id:3, name:"Shehryar",email:"shehryar9@gmail.com", contact:"+92 310 8967211",role:"member"
+            id:3, name:"Shehryar",email:"shehryar9@gmail.com", contact:"+92 310 8967211",role:"Manager"
          },
          {
-            id:4, name:"Mohsin",email:"mohsin89@gmail.com", contact:"+92 310 5027211",role:"member"
+            id:4, name:"Mohsin",email:"mohsin89@gmail.com", contact:"+92 310 5027211",role:"Manager"
          }
     ],
     newMember:{
-        name:"",email:"",role:""
+        name:"",email:"",role:"Admin"
     }
 }
 
 export default(state=initialstate,action)=>{
 switch(action.type){
+    
          case Add_Member:
+                var id=state.members.length+1;
+                var newmember={
+                    id:id,
+                    ...state.newMember,
+                    contact:"+92 310 5027211"
+                }
+             
         return{
-            ...state
+            ...state,
+            members:[
+                ...state.members,
+                newmember
+        ],
+        newMember:{
+            name:"",email:"",role:"Admin"
+        }
+
         }
         case Remove_Member:
                 return{
@@ -31,11 +47,12 @@ switch(action.type){
                     members:state.members.filter(member=>member.id!==action.id)
         }
         case SET_NEW_MEMBER:
+        
             return {
                 ...state,
-                newmember:{
+                newMember:{
                     ...state.newMember,
-                    [action.key]:state.newMember.name+[action.value]
+                    [action.key]:action.value
                 }
             }
     default:
